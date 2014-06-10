292a3ceddc68cc533612456f71a4d2f862431bb1
Jim Capozzoli <saltmiser@gmail.com>
2014-06-10 11:44:25 -0400

    Switched to Ruby.  Notice how object oriented code looks better in Ruby.

--- slopefinder.rb
+++ slopefinder.rb
@@ -1,2 +1,33 @@
-# Hello
-print "hello"
+
+datadir = './data/'
+
+class DataPointFromFile
+  def initialize(text_file_line)
+    @data_array = text_file_line.split
+  end
+  def get_columns
+    @data_array
+  end
+  def to_s
+    "Data Point with " + get_columns.count.to_s + " columns"
+  end 
+end
+  
+
+datapoints = []
+
+File.open(datadir + 'LHIP 240.txt') do |infile|
+  skiplines = 0
+  while line = infile.gets
+    if skiplines < 7
+      skiplines = skiplines + 1
+    else 
+      datapoints << DataPointFromFile.new(line)
+    end
+  end
+end
+
+puts datapoints[0]
+puts "Total rows in data file: " + datapoints.count.to_s
+
+
